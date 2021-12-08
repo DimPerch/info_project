50e0c13b793fb6a483a758df925021a0ec03f53a
Dimitry Perchatkin <perchatkindimitry@gmail.com>
2021-12-09 00:50:52 +0300

    version-2 (Появился первый дизайн)

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from app import App
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QFontDatabase
 
 
-app = QtWidgets.QApplication([])
-application = App()
-application.show()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
 
+    font = QFontDatabase.addApplicationFont('Fairfax.ttf')
+    if font == -1:
+        raise FileNotFoundError("failed to connect font")
 
-sys.exit(app.exec())
+    file = QtCore.QFile(r"style.qss")
+    file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text)
+    stream = QtCore.QTextStream(file)
+    app.setStyleSheet(stream.readAll())
+
+    application = App()
+    application.show()
+
+    sys.exit(app.exec())
